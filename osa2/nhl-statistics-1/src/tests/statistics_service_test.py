9df17b69a67be4d01eac4d2ef17a17bef185e2ff
lvvuolle <lvvuolle@jyu.fi>
2025-11-08 14:49:24 +0200

    Unittestit lisatty statistics_service luokalle

---
+++ osa2/nhl-statistics-1/src/tests/statistics_service_test.py
@@ -0,0 +1,30 @@
+import unittest
+from statistics_service import StatisticsService
+from statistics_service import PlayerReader
+from player import Player
+
+class PlayerReaderStub:
+    def get_players(self):
+        return [
+            Player("Semenko", "EDM", 4, 12),
+            Player("Lemieux", "PIT", 45, 54),
+            Player("Kurri",   "EDM", 37, 53),
+            Player("Yzerman", "DET", 42, 56),
+            Player("Gretzky", "EDM", 35, 89)
+        ]
+
+class TestStatisticsService(unittest.TestCase):
+    def setUp(self):
+        # annetaan StatisticsService-luokan oliolle "stub"-luokan olio
+        self.stats = StatisticsService(
+            PlayerReaderStub()
+        )
+
+    def test_search(self):
+        self.assertEqual(self.stats.search("Semenko"), Player("Semenko", "EDM", 4, 12))
+
+    def test_team(self):
+        self.assertEqual(self.stats.team("EDM"), [Player("Semenko", "EDM", 4, 12), Player("Kurri",   "EDM", 37, 53), Player("Gretzky", "EDM", 35, 89)])
+
+    def test_top(self):
+        self.assertEqual(self.stats.top(2), [Player("Gretzky", "EDM", 35, 89), Player("Yzerman", "DET", 42, 56)])
